f7d1906fe8fbe350ca19f63a7788232cea2b55b5
Shlok Natarajan <shlok.natarajan@gmail.com>
2025-05-29 14:09:34 -0700

    feat: absolute to relative imports

--- src/markdown_from_pmid.py
+++ src/markdown_from_pmid.py
@@ -9,9 +9,9 @@ from typing import List, Optional
 from loguru import logger
 import pandas as pd
 import os
-from src.pmcid_from_pmid import batch_pmcid_from_pmid
-from src.html_from_pmcid import get_html_from_pmcid
-from src.markdown_from_html import html_to_markdown
+from .pmcid_from_pmid import batch_pmcid_from_pmid
+from .html_from_pmcid import get_html_from_pmcid
+from .markdown_from_html import html_to_markdown
 import argparse
 import tqdm
 
